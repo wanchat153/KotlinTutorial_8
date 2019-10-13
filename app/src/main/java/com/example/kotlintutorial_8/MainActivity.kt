f541ab0e1d8565ead9a32de94c6a8b980ced801b
Wanchat <wanchatgame@gmail.com>
2019-10-13 19:23:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kotlintutorial_8/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlintutorial_8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
