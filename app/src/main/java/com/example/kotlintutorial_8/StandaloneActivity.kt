2dcfbd92ab8704dea0245900bb31249f9353decd
Wanchat <wanchatgame@gmail.com>
2019-10-13 21:40:02 +0700

    1. ลงปลั๊กอืน YouTubeAndroidPlayerApi
    2.
    สร้าง Youtube Activity
    
    3.
    เปลี่ยนชื่อ layout และ add widgets dynamically
    
    4.
    สร้าง keys และเอา keys ที่ได้จาก youtube api ใส่
    ใส่ url youtube
    
    5.
    ประกาศใช่ key api youtube
    
    6.
    onInitializationSuccess method
    
    7.
    add Listeners
    
    8.
    แก้ไขการเล่น video
    
    9.
    ใส่ Button
    สร้าง activity_standalone
    
    10.
    Youyube fun onclick code
    
    11.
    ตั้งค่า manifast, intents
    
    12.
    เอา key มาใส่ใน fun onClick

--- app/src/main/java/com/example/kotlintutorial_8/StandaloneActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_8/StandaloneActivity.kt
@@ -33,9 +33,9 @@ class StandaloneActivity : AppCompatActivity(), View.OnClickListener{
     override fun onClick(view: View?) {
         val intent = when (view?.id){
             R.id.btnPlayVideo -> YouTubeStandalonePlayer.createVideoIntent(
-                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_VIDEO_ID)
+                this, "AIzaSyCbiVioHtj_nlDFNfUz6TKYzliucIOGHXs", YOUTUBE_VIDEO_ID, 0, true, false)
             R.id.btnPlaylist -> YouTubeStandalonePlayer.createPlaylistIntent(
-                this, getString(R.string.GOOGLE_API_KEY), YOUTUBE_PLAYLIST)
+                this, "AIzaSyCbiVioHtj_nlDFNfUz6TKYzliucIOGHXs", YOUTUBE_PLAYLIST, 0, 0, true, true)
             else -> throw IllegalArgumentException("Undefind button clicked")
         }
         startActivity(intent)
