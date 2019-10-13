09a03f51e35ae7da9d9b9b7362ac0701c24e1b52
Wanchat <wanchatgame@gmail.com>
2019-10-13 19:41:13 +0700

    1. ลงปลั๊กอืน YouTubeAndroidPlayerApi
    2.
    สร้าง Youtube Activity

---
+++ app/src/main/java/com/example/kotlintutorial_8/YouTubeActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kotlintutorial_8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+
+class YouTubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_you_tube)
+    }
+
+    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
