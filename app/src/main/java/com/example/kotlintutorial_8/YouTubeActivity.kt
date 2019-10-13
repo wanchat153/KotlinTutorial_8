e9c9a1710bbb0987d5e4977a9bde14c51af3eb3c
Wanchat <wanchatgame@gmail.com>
2019-10-13 19:57:34 +0700

    1. ลงปลั๊กอืน YouTubeAndroidPlayerApi
    2.
    สร้าง Youtube Activity
    
    3.
    เปลี่ยนชื่อ layout และ add widgets dynamically

--- app/src/main/java/com/example/kotlintutorial_8/YouTubeActivity.kt
+++ app/src/main/java/com/example/kotlintutorial_8/YouTubeActivity.kt
@@ -1,16 +1,32 @@
 package com.example.kotlintutorial_8
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class YouTubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_you_tube)
+//        setContentView(R.layout.activity_youtube)
+//        val layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null)as ConstraintLayout
+        setContentView(layout)
+
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600,100)
+//        button1.text = "Buttton added"
+//        layout.addView(button1)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
